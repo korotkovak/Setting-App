2bc82682a0b2503b9996eb12ff9587c2390ab863
korotkovak <kristinakorotkova059@gmail.com>
2023-01-19 22:08:10 +0800

    Create class SettingsScreenController

---
+++ Setting App/Sources/Scenes/Controller/SettingsScreenController.swift
@@ -0,0 +1,11 @@
+//
+//  SettingsScreenController.swift
+//  Setting App
+//
+//  Created by Kristina Korotkova on 19/01/23.
+//
+
+import UIKit
+
+final class SettingsScreenController: UIViewController {
+}
